455898f91f8ae08e87beeafbe0abf8541e1ff0b5
Cenker Kumlucalı <user3@local.lum>
2022-04-28 12:09:12 +0300

    first commit

---
+++ GoogleAuthenticator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using OtpNet;
+
+namespace GoogleAuthenticator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string base32Secret = "L52AM3FD452MARQQQPT54N6XVAJK6JR7";
+            var secret = Base32Encoding.ToBytes(base32Secret);
+
+            var totp = new Totp(secret);
+
+            var usedTimeSteps = new HashSet<long>();
+
+            while (true)
+            {
+                Console.Write("Enter code: ");
+                string inputCode = Console.ReadLine();
+                bool valid = totp.VerifyTotp(inputCode, out long timeStepMatched,
+                    VerificationWindow.RfcSpecifiedNetworkDelay);
+
+                valid &= !usedTimeSteps.Contains(timeStepMatched);
+                usedTimeSteps.Add(timeStepMatched);
+
+                string validStr = valid ? "Valid" : "Invalid";
+                var colour = valid ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.ForegroundColor = colour;
+                Console.WriteLine(validStr);
+                Console.ResetColor();
+            }
+        }
+    }
+}
